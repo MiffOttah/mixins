1bb2c1c7b04caa853f502c1f42c16f3adc5d0cd7
Miff <miff@miffthefox.info>
2019-07-09 22:51:25 -0400

    Add DisposableCollection

---
+++ MiffTheFox.Mixins/DisposableCollection.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Collections
+{
+    internal class DisposableCollection : ICollection<IDisposable>, IDisposable
+    {
+        private List<IDisposable> _Items = new List<IDisposable>();
+        private bool _Disposed = false; // To detect redundant calls
+
+        public int Count => _Items.Count;
+        public bool IsReadOnly => ((ICollection<IDisposable>)_Items).IsReadOnly;
+
+        public void Add(IDisposable item)
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            if (_Index(item) != -1) throw new ArgumentException("This item is already in the collection.", nameof(item));
+
+            _Items.Add(item);
+        }
+
+        public void Clear() => Clear(true);
+
+        public void Clear(bool dispose)
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            if (dispose)
+            {
+                foreach (var i in _Items) i.Dispose();
+            }
+            _Items.Clear();
+        }
+
+        private int _Index(IDisposable item)
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            for (int i = 0; i < _Items.Count; i++)
+            {
+                if (ReferenceEquals(_Items[i], item)) return i;
+            }
+            return -1;
+        }
+
+        public bool Contains(IDisposable item) => _Index(item) != -1;
+
+        public void CopyTo(IDisposable[] array, int arrayIndex)
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            _Items.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<IDisposable> GetEnumerator()
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            return ((ICollection<IDisposable>)_Items).GetEnumerator();
+        }
+
+        public bool Remove(IDisposable item) => Remove(item, true);
+
+        public bool Remove(IDisposable item, bool dispose)
+        {
+            int i = _Index(item);
+            if (i != -1)
+            {
+                if (dispose)
+                {
+                    _Items[i].Dispose();
+                }
+                _Items.RemoveAt(i);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            if (_Disposed) throw new ObjectDisposedException(null);
+            return ((ICollection<IDisposable>)_Items).GetEnumerator();
+        }
+
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_Disposed)
+            {
+                if (disposing)
+                {
+                    Clear();
+                }
+
+                _Items = null;
+                _Disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ MixinTests/DisposbleCollectionTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox;
+using MiffTheFox.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests
+{
+    [TestClass]
+    public class DisposbleCollectionTests
+    {
+        class MockDisposable : IDisposable
+        {
+            public bool Disposed { get; private set; } = false;
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+        }
+
+        [TestMethod]
+        public void TestMock()
+        {
+            var d1 = new MockDisposable();
+            Assert.IsFalse(d1.Disposed);
+            d1.Dispose();
+            Assert.IsTrue(d1.Disposed);
+        }
+
+        [TestMethod]
+        public void TestBasicDisposal()
+        {
+            var d1 = new MockDisposable();
+            var d2 = new MockDisposable();
+
+            Assert.IsFalse(d1.Disposed);
+            Assert.IsFalse(d2.Disposed);
+
+            var coll = new DisposableCollection { d1, d2 };
+            coll.Dispose();
+
+            Assert.IsTrue(d1.Disposed);
+            Assert.IsTrue(d2.Disposed);
+        }
+
+        [TestMethod]
+        public void TestObjectDisposedException()
+        {
+            var c = new DisposableCollection();
+
+            c.Dispose();
+            c.Dispose(); // second call should have no effect
+
+            Assert.ThrowsException<ObjectDisposedException>(() =>
+                c.Add(new MockDisposable()));
+
+            Assert.ThrowsException<ObjectDisposedException>(() =>
+                c.Clear());
+
+            Assert.ThrowsException<ObjectDisposedException>(() =>
+                c.Contains(new MockDisposable()));
+
+            Assert.ThrowsException<ObjectDisposedException>(() =>
+                c.GetEnumerator());
+
+            Assert.ThrowsException<ObjectDisposedException>(() =>
+                c.Remove(new MockDisposable()));
+        }
+
+        [TestMethod]
+        public void TestInvalidOperations()
+        {
+            var c = new DisposableCollection();
+
+            var d1 = new MockDisposable();
+
+            // adding a reference to the same object twice
+            c.Add(d1);
+            Assert.ThrowsException<ArgumentException>(() =>
+                c.Add(d1));
+
+            // adding a null reference
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                c.Add(null));
+        }
+
+        [TestMethod]
+        public void TestDisposeOnRemove()
+        {
+            (DisposableCollection, MockDisposable) prepareSet()
+            {
+                var d = new MockDisposable();
+                var c = new DisposableCollection { d };
+                return (c, d);
+            }
+
+            var (c1, d1) = prepareSet();
+            c1.Remove(d1);
+            Assert.IsTrue(d1.Disposed);
+
+            (c1, d1) = prepareSet();
+            c1.Remove(d1, true);
+            Assert.IsTrue(d1.Disposed);
+
+            (c1, d1) = prepareSet();
+            c1.Remove(d1, false);
+            Assert.IsFalse(d1.Disposed);
+        }
+
+        [TestMethod]
+        public void TestDisposeOnClear()
+        {
+            (DisposableCollection, MockDisposable) prepareSet()
+            {
+                var d = new MockDisposable();
+                var c = new DisposableCollection { d };
+                return (c, d);
+            }
+
+            var (c1, d1) = prepareSet();
+            c1.Clear();
+            Assert.IsTrue(d1.Disposed);
+
+            (c1, d1) = prepareSet();
+            c1.Clear(true);
+            Assert.IsTrue(d1.Disposed);
+
+            (c1, d1) = prepareSet();
+            c1.Clear(false);
+            Assert.IsFalse(d1.Disposed);
+        }
+    }
+}
