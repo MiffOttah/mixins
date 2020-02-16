701c7a083f0c21bc1e0122fefc3517b3552d05b0
Miff <miff@miffthefox.info>
2020-02-16 18:01:10 -0500

    Add (unfinished) truthiness

---
+++ MiffTheFox.Mixins/Truthiness.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// WARNING - not ready for produciton use
+
+namespace MiffTheFox
+{
+    internal static class Truthiness
+    {
+        public static bool IsTruthy(this object obj)
+        {
+            // note: nullables will pattern-match as null if
+            // lacking a value, or will pattern-match to the underlying
+            // type if possessing a value. thus, we can treat a nullable
+            // with a value with the same mechanisms as for the underlying
+            // type
+
+            switch (obj)
+            {
+                case null:
+                case System.DBNull _:
+                    // null values are always falsey
+                    return false;
+
+                case string stringValue:
+                    // the null case is handled above
+                    // so only check if the string is empty
+                    return stringValue.Length > 0;
+
+                // integer values are truthy if nonzero
+                case sbyte n: return n != 0;
+                case byte n: return n != 0;
+                case short n: return n != 0;
+                case ushort n: return n != 0;
+                case int n: return n != 0;
+                case uint n: return n != 0U;
+                case long n: return n != 0L;
+                case ulong n: return n != 0UL;
+
+                // floating point values are truthy if nonzero and not nan
+                case float f: return !float.IsNaN(f) && f != 0f;
+                case double f: return !double.IsNaN(f) && f != 0.0;
+
+                case decimal dec:
+                    // decimal values are truthy if nonzero
+                    return dec != 0M;
+
+                case char ch:
+                    // chars are truthy if not NUL
+                    return ch != '\0';
+
+                case Enum e:
+                    // todo: default enum value
+                    throw new NotImplementedException();
+
+                case System.Collections.ICollection collection:
+                    // todo: collection length
+                    throw new NotImplementedException();
+
+                // xxx: are there other types to check?
+
+                default:
+                    return true;
+            }
+        }
+    }
+}
---
+++ MixinTests/TruthinessTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests
+{
+    [TestClass]
+    public class TruthinessTests
+    {
+        [TestMethod]
+        public void TestNullTruthiness()
+        {
+            Assert.IsFalse(Truthiness.IsTruthy(null));
+            Assert.IsFalse(Truthiness.IsTruthy((int?)null));
+            Assert.IsFalse(Truthiness.IsTruthy(DBNull.Value));
+        }
+
+        [TestMethod]
+        [DataRow(true, "ABC")]
+        [DataRow(true, "1234567890")]
+        [DataRow(true, "0")] // this ain't javascript.
+        [DataRow(false, "")]
+        [DataRow(false, null)]
+        public void TestStringTruthiness(bool expected, string value)
+        {
+            Assert.AreEqual(expected, Truthiness.IsTruthy(value));
+        }
+
+        // fixme: this test is currently failing
+        // determine which step is failing and address why
+        [TestMethod]
+        public void TestIntegerTruthiness()
+        {
+            Assert.IsTrue(Truthiness.IsTruthy((byte)1));
+            Assert.IsFalse(Truthiness.IsTruthy((byte)0));
+
+            Assert.IsTrue(Truthiness.IsTruthy((sbyte)1));
+            Assert.IsTrue(Truthiness.IsTruthy((sbyte)-1));
+            Assert.IsFalse(Truthiness.IsTruthy((sbyte)0));
+
+            Assert.IsTrue(Truthiness.IsTruthy((ushort)1));
+            Assert.IsFalse(Truthiness.IsTruthy((ushort)0));
+
+            Assert.IsTrue(Truthiness.IsTruthy((short)1));
+            Assert.IsTrue(Truthiness.IsTruthy((short)-1));
+            Assert.IsFalse(Truthiness.IsTruthy((short)0));
+
+            Assert.IsTrue(Truthiness.IsTruthy(1U));
+            Assert.IsFalse(Truthiness.IsTruthy(1U));
+
+            Assert.IsTrue(Truthiness.IsTruthy(1));
+            Assert.IsTrue(Truthiness.IsTruthy(-1));
+            Assert.IsFalse(Truthiness.IsTruthy(0));
+
+            Assert.IsTrue(Truthiness.IsTruthy(1UL));
+            Assert.IsFalse(Truthiness.IsTruthy(1UL));
+
+            Assert.IsTrue(Truthiness.IsTruthy(1L));
+            Assert.IsTrue(Truthiness.IsTruthy(-1L));
+            Assert.IsFalse(Truthiness.IsTruthy(0L));
+        }
+
+        [TestMethod]
+        public void TestSingleTruthiness()
+        {
+            Assert.IsTrue(Truthiness.IsTruthy(1f));
+            Assert.IsTrue(Truthiness.IsTruthy(-1f));
+            Assert.IsTrue(Truthiness.IsTruthy(float.PositiveInfinity));
+            Assert.IsFalse(Truthiness.IsTruthy(0f));
+            Assert.IsFalse(Truthiness.IsTruthy(float.NaN));
+        }
+
+        [TestMethod]
+        public void TestDoubleTruthiness()
+        {
+            Assert.IsTrue(Truthiness.IsTruthy(1.0));
+            Assert.IsTrue(Truthiness.IsTruthy(-1.0));
+            Assert.IsTrue(Truthiness.IsTruthy(double.PositiveInfinity));
+            Assert.IsFalse(Truthiness.IsTruthy(0.0));
+            Assert.IsFalse(Truthiness.IsTruthy(double.NaN));
+        }
+
+        [TestMethod]
+        public void TestDecimalTruthiness()
+        {
+            Assert.IsTrue(Truthiness.IsTruthy(1M));
+            Assert.IsTrue(Truthiness.IsTruthy(-1M));
+            Assert.IsFalse(Truthiness.IsTruthy(0M));
+        }
+
+        [TestMethod]
+        public void TestChartruthiness()
+        {
+            Assert.IsTrue(Truthiness.IsTruthy('A'));
+            Assert.IsFalse(Truthiness.IsTruthy('\0'));
+        }
+    }
+}
