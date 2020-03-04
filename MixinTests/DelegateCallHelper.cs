5a111696600e223fb3ef8e29e5f69d6241ef2170
Miff <miff@miffthefox.info>
2020-03-04 18:19:48 -0500

    Move DelegateCallHelper to its own file?

---
+++ MixinTests/DelegateCallHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MixinTests
+{
+    // for testing if a method is called
+    class DelegateCallHelper
+    {
+        public bool Called { get; private set; } = false;
+
+        public void Call()
+        {
+            Called = true;
+        }
+
+        public T Call<T>(T value)
+        {
+            Called = true;
+            return value;
+        }
+
+        public void AssertCalled() => Assert.IsTrue(Called);
+        public void AssertNotCalled() => Assert.IsFalse(Called);
+        public void AssertCalled(bool expected) => Assert.AreEqual(expected, Called);
+        public void Reset() => Called = false;
+
+        //public static void AssertCall(Action<DelegateCallHelper> test)
+        //{
+        //    var h = new DelegateCallHelper();
+        //    test(h);
+        //    h.AssertCalled();
+        //}
+
+        //public static void AssertNotCall(Action<DelegateCallHelper> test)
+        //{
+        //    var h = new DelegateCallHelper();
+        //    test(h);
+        //    h.AssertNotCalled();
+        //}
+    }
+}
--- MixinTests/TruthinessExtensionsTests.cs
+++ MixinTests/TruthinessExtensionsTests.cs
@@ -11,42 +11,6 @@ namespace MixinTests
     [TestClass]
     public class TruthinessExtensionsTests
     {
-        // for testing if a method is called
-        class DelegateCallHelper
-        {
-            public bool Called { get; private set; } = false;
-
-            public void Call()
-            {
-                Called = true;
-            }
-
-            public T Call<T>(T value)
-            {
-                Called = true;
-                return value;
-            }
-
-            public void AssertCalled() => Assert.IsTrue(Called);
-            public void AssertNotCalled() => Assert.IsFalse(Called);
-            public void AssertCalled(bool expected) => Assert.AreEqual(expected, Called);
-            public void Reset() => Called = false;
-
-            //public static void AssertCall(Action<DelegateCallHelper> test)
-            //{
-            //    var h = new DelegateCallHelper();
-            //    test(h);
-            //    h.AssertCalled();
-            //}
-
-            //public static void AssertNotCall(Action<DelegateCallHelper> test)
-            //{
-            //    var h = new DelegateCallHelper();
-            //    test(h);
-            //    h.AssertNotCalled();
-            //}
-        }
-
         [TestMethod]
         [DataRow(true, 1)]
         [DataRow(true, "value")]
