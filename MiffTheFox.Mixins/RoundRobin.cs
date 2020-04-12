5ceab3eef5978d0bcd82bd47b1318a46b3ceb10b
Miff <miff@miffthefox.info>
2020-04-12 17:21:45 -0400

    Add RoundRobin<T>

---
+++ MiffTheFox.Mixins/RoundRobin.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiffTheFox.Collections
+{
+    public class RoundRobin<T> : ICollection<T>, ICollection
+    {
+        private readonly T[] _Items;
+        private int _ItemIndex;
+
+        public int MaximumItems => _Items.Length;
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+        bool ICollection.IsSynchronized => false;
+        object ICollection.SyncRoot => this;
+
+        public RoundRobin(int maximumItems)
+        {
+            if (maximumItems <= 0) throw new ArgumentOutOfRangeException(nameof(maximumItems), "Maximum items must be greater than zero.");
+            _Items = new T[maximumItems];
+            _ItemIndex = 0;
+            Count = 0;
+        }
+
+        protected IEnumerable<T> Enumerate()
+        {
+            for (int i = 0; i < MaximumItems; i++)
+            {
+                // add i to _ItemIndex and wrap around
+                // so that the enumeration starts with the
+                // oldest item
+                int i2 = (i + _ItemIndex) % MaximumItems;
+
+                // terminate early if the array isn't full
+                if (i2 >= Count) continue;
+                
+                // yield this item
+                yield return _Items[i2];
+            }
+        }
+
+        public void Add(T item)
+        {
+            _Items[_ItemIndex] = item;
+            if (Count < MaximumItems) Count++;
+
+            _ItemIndex++;
+            if (_ItemIndex >= MaximumItems) _ItemIndex = 0;
+        }
+
+        public void Clear()
+        {
+            _ItemIndex = 0;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return Enumerate().Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (arrayIndex + Count > array.Length) throw new ArgumentException("The number of elements in the source ICollection<T> is greater than the available space from arrayIndex to the end of the destination array.");
+
+            int i = 0;
+            foreach (var item in Enumerate())
+            {
+                array[i + arrayIndex] = item;
+                i++;
+            }
+        }
+
+        void ICollection.CopyTo(Array array, int index)
+        {
+            if (array is null) throw new ArgumentNullException(nameof(array));
+            if (array.Rank != 1) throw new ArgumentException("Array is multidimensional.", nameof(array));
+            if (index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+            if (index + Count > array.Length) throw new ArgumentException("The number of elements in the source ICollection<T> is greater than the available space from arrayIndex to the end of the destination array.");
+            if (!array.GetType().GetElementType().IsAssignableFrom(typeof(T))) throw new ArgumentException("The type of the source ICollection cannot be cast automatically to the type of the destination array.", nameof(array));
+
+            var values = ToArray();
+            Array.Copy(values, 0, array, index, values.Length);
+        }
+
+        public T[] ToArray()
+        {
+            var result = new T[Count];
+            int i = 0;
+            foreach (var item in Enumerate())
+            {
+                result[i++] = item;
+            }
+            return result;
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public IEnumerator<T> GetEnumerator() => Enumerate().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => Enumerate().GetEnumerator();
+    }
+}
---
+++ MixinTests/RoundRobinTests.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox;
+using MiffTheFox.Collections;
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+
+namespace MixinTests
+{
+    [TestClass]
+    public class RoundRobinTests
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var rr = new RoundRobin<int>(10);
+            Assert.AreEqual(10, rr.MaximumItems);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-10)]
+        public void TestCreateFail(int maximumItems)
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new RoundRobin<int>(maximumItems));
+        }
+
+        [TestMethod]
+        public void TestEmpty()
+        {
+            var rr = new RoundRobin<int>(10);
+            Assert.AreEqual(0, rr.Count);
+            foreach (int item in rr)
+            {
+                Assert.Fail("Enumeration returned values.");
+            }
+        }
+
+        [TestMethod]
+        public void TestPartialFill()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 5; i++)
+            {
+                rr.Add(i);
+            }
+
+            Assert.AreEqual(5, rr.Count);
+            CollectionAssert.AreEqual(new int[] { 0, 1, 2, 3, 4 }, rr);
+        }
+
+        [TestMethod]
+        public void TestOverfill()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 15; i++)
+            {
+                rr.Add(i);
+            }
+            Assert.AreEqual(10, rr.Count);
+            CollectionAssert.AreEqual(new int[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, rr);
+        }
+
+        [TestMethod]
+        [DataRow("1 2 3 4 5", "1 2 3 4 5")]
+        [DataRow("1 2 3 4 5 6 7 8 9 10", "1 2 3 4 5 6 7 8 9 10")]
+        [DataRow("6 7 8 9 10 11 12 13 14 15", "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15")]
+        public void TestEnumerate(string expected, string data)
+        {
+            int[] expectedInts = expected.Split(' ').Select(d => int.Parse(d, CultureInfo.InvariantCulture)).ToArray();
+            int[] dataInts = data.Split(' ').Select(d => int.Parse(d, CultureInfo.InvariantCulture)).ToArray();
+
+            var rr = new RoundRobin<int>(10);
+            foreach (var di in dataInts) rr.Add(di);
+
+            var enumerated = rr.Select(x => x).ToArray(); // use linq to enumerate the roundrobin
+            CollectionAssert.AreEqual(expectedInts, enumerated);
+
+            var enumeratedCount = rr.Select(x => x).Count();
+            Assert.AreEqual(rr.Count, enumeratedCount);
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 5; i++)
+            {
+                rr.Add(i);
+            }
+            Assert.AreEqual(5, rr.Count);
+
+            rr.Clear();
+            Assert.AreEqual(0, rr.Count);
+            CollectionAssert.AreEquivalent(new int[] { }, rr);
+        }
+
+        [TestMethod]
+        public void TestToArray()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 15; i++)
+            {
+                rr.Add(i);
+            }
+            CollectionAssert.AreEqual(new int[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, rr.ToArray());
+        }
+
+        [TestMethod]
+        public void TestCopyTo()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 15; i++)
+            {
+                rr.Add(i);
+            }
+
+            var test = new int[10];
+            rr.CopyTo(test, 0);
+            CollectionAssert.AreEqual(new int[] { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, test);
+        }
+
+        [TestMethod]
+        public void TestCopyToMiddle()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 15; i++)
+            {
+                rr.Add(i);
+            }
+
+            var test = new int[12];
+            rr.CopyTo(test, 2);
+            CollectionAssert.AreEqual(new int[] { 0, 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, test);
+        }
+
+        [TestMethod]
+        public void TestCopyToPartial()
+        {
+            var rr = new RoundRobin<int>(10);
+            for (int i = 0; i < 5; i++)
+            {
+                rr.Add(i);
+            }
+
+            var test = new int[10];
+            rr.CopyTo(test, 0);
+            CollectionAssert.AreEqual(new int[] { 0, 1, 2, 3, 4, 0, 0, 0, 0, 0 }, test);
+        }
+
+        [TestMethod]
+        public void TestCopyToFail()
+        {
+            var rr = new RoundRobin<int>(10)
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            var array5 = new int[5];
+            var array10 = new int[10];
+
+            Assert.ThrowsException<ArgumentNullException>(() => rr.CopyTo(null, 0));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => rr.CopyTo(array10, -1));
+            Assert.ThrowsException<ArgumentException>(() => rr.CopyTo(array5, 0));
+            Assert.ThrowsException<ArgumentException>(() => rr.CopyTo(array10, 3));
+        }
+
+        [TestMethod]
+        public void TestRemoveNotSupported()
+        {
+            var rr = new RoundRobin<int>(10)
+            {
+                123,
+                456
+            };
+
+            Assert.ThrowsException<NotSupportedException>(() => rr.Remove(123));
+        }
+
+        [TestMethod]
+        public void TestICollectionCopyTo()
+        {
+            ICollection rr = new RoundRobin<int>(10)
+            {
+                123,
+                456
+            };
+            int[] res = new int[2];
+
+            rr.CopyTo(res, 0);
+            CollectionAssert.AreEqual(new int[] { 123, 456 }, res);
+        }
+
+        [TestMethod]
+        public void TestICollectionCopyToFail()
+        {
+            var rr = new RoundRobin<int>(10)
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var iCollection = (ICollection)rr;
+            var array5 = new int[5];
+            var array10 = new int[10];
+            var invalidArrayType = new string[10];
+
+            Assert.ThrowsException<ArgumentNullException>(() => iCollection.CopyTo(null, 0));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => iCollection.CopyTo(array10, -1));
+            Assert.ThrowsException<ArgumentException>(() => iCollection.CopyTo(array5, 0));
+            Assert.ThrowsException<ArgumentException>(() => iCollection.CopyTo(array10, 3));
+            Assert.ThrowsException<ArgumentException>(() => iCollection.CopyTo(invalidArrayType, 0));
+        }
+    }
+}
