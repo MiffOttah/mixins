0e4f722c842cc6f70df20b0d5be4e0593d29177f
Miff <miff@miffthefox.info>
2020-04-15 17:53:03 -0400

    Add EmptyList<T>

---
+++ MiffTheFox.Mixins/Collections/EmptyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Collections
+{
+    /// <summary>
+    /// Repersents a list of <typeparamref name="T"/> that will never contain any items.
+    /// </summary>
+    internal class EmptyList<T> : IList<T>
+    {
+        // The static shared list is initilized the first time the type is
+        // referenced for a particular T.
+        public static EmptyList<T> Shared { get; } = new EmptyList<T>();
+
+        public int Count => 0;
+        public bool IsReadOnly => true;
+
+        public T this[int index]
+        {
+            // always throw the IndexOutOfRangeException since there are no
+            // possible valid indicies.
+            get => throw new IndexOutOfRangeException();
+            set => throw new IndexOutOfRangeException();
+        }
+
+        public EmptyList()
+        {
+        }
+
+        // the list will never contain anything
+        public bool Contains(T item) => false;
+        public int IndexOf(T item) => -1;
+        public bool Remove(T item) => false;
+
+        // being read-only, the list will always throw NotSupportedException
+        // in response to attempts to modify it.
+        public void Add(T item) => throw new NotSupportedException();
+        public void Insert(int index, T item) => throw new NotSupportedException();
+        public void Clear() => throw new NotSupportedException();
+
+        // as with the accessor, there are no valid indicies into the list
+        public void RemoveAt(int index) => throw new ArgumentOutOfRangeException();
+
+        // technically this is copying nothing into the array, so this is a no-op
+        public void CopyTo(T[] array, int arrayIndex) { }
+        
+        public IEnumerator<T> GetEnumerator() => new EmptyEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => new EmptyEnumerator();
+
+        public class EmptyEnumerator : IEnumerator<T>
+        {
+            public T Current => default;
+            object IEnumerator.Current => default(T);
+            public void Dispose() { }
+            public bool MoveNext() => false;
+            public void Reset() { }
+        }
+    }
+}
---
+++ MixinTests/EmptyListTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox.Collections;
+
+namespace MixinTests
+{
+    [TestClass]
+    public class EmptyListTests
+    {
+        // these tests are dead-simple
+        // since the EmptyList<T> ignores all input
+        // and saves no state
+
+        [TestMethod]
+        public void TestCount()
+        {
+            Assert.AreEqual(0, EmptyList<int>.Shared.Count);
+        }
+
+        [TestMethod]
+        public void TestIsReadOnly()
+        {
+            Assert.AreEqual(true, EmptyList<int>.Shared.IsReadOnly);
+        }
+
+        [TestMethod]
+        public void TestGetByIndex()
+        {
+            Assert.ThrowsException<IndexOutOfRangeException>(
+                () => EmptyList<int>.Shared[0]
+            );
+        }
+
+        [TestMethod]
+        public void TestSetByIndex()
+        {
+            Assert.ThrowsException<IndexOutOfRangeException>(
+                () => EmptyList<int>.Shared[0] = 1
+            );
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            Assert.IsFalse(EmptyList<int>.Shared.Contains(0));
+        }
+
+        [TestMethod]
+        public void TestIndexOf()
+        {
+            Assert.AreEqual(-1, EmptyList<int>.Shared.IndexOf(0));
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            Assert.IsFalse(EmptyList<int>.Shared.Remove(0));
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.ThrowsException<NotSupportedException>(
+                () => EmptyList<int>.Shared.Add(0)
+            );
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            Assert.ThrowsException<NotSupportedException>(
+                () => EmptyList<int>.Shared.Insert(0, 0)
+            );
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            Assert.ThrowsException<NotSupportedException>(
+                () => EmptyList<int>.Shared.Clear()
+            );
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => EmptyList<int>.Shared.RemoveAt(0)
+            );
+        }
+
+        [TestMethod]
+        [DataRow("abc,123,def", 0)]
+        [DataRow("w,x,y,z", 2)]
+        [DataRow("", 0)]
+        [DataRow("abc,123,def", -1)]
+        [DataRow("abc,123,def", 100)]
+        public void TestCopyTo(string testData, int index)
+        {
+            var expected = testData.Split(',');
+            var actual = testData.Split(',');
+            EmptyList<string>.Shared.CopyTo(actual, index);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestEnumerate()
+        {
+            foreach (int item in EmptyList<int>.Shared)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
