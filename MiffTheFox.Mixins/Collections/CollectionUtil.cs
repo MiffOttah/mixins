17614a138dcecc7cbaeb7d8616c73cf87534e9a6
Miff <miff@miffthefox.info>
2020-08-17 19:38:14 -0400

    Add CollectionUtil.Concat

---
+++ MiffTheFox.Mixins/Collections/CollectionUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiffTheFox.Collections
+{
+    public static class CollectionUtil
+    {
+        public static T[] Concat<T>(params ICollection<T>[] collections)
+        {
+            if (collections is null)
+            {
+                throw new ArgumentNullException(nameof(collections));
+            }
+
+            int count = 0;
+            for (int i = 0; i < collections.Length; i++)
+            {
+                if (collections[i] != null)
+                {
+                    count += collections[i].Count;
+                }
+            }
+
+            var result = new T[count];
+            int position = 0;
+
+            for (int i = 0; i < collections.Length; i++)
+            {
+                if (collections[i] != null)
+                {
+                    collections[i].CopyTo(result, position);
+                    position += collections[i].Count;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MixinTests/Collections/CollectionUtilTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests.Collections
+{
+    [TestClass]
+    public class CollectionUtilTests
+    {
+        [TestMethod]
+        [DataRow("1,2,3,4", "1,2;3,4")]
+        [DataRow("A,B,C", "A;B;C")]
+        [DataRow("A,B,C,D", "A,B,C,D", DisplayName = "With single element")]
+        [DataRow("1,2,3", "1;;2,3", DisplayName = "With empty element")]
+        [DataRow("1,2,3", "1;null;2,3", DisplayName = "With null element")]
+        [DataRow("", null, DisplayName = "With no elements")]
+        [DataRow("", ";;null", DisplayName = "With only empty/null elements")]
+        public void ConcatTest(string expected, string sources)
+        {
+            var l = new List<string[]>();
+            if (sources != null)
+            {
+                foreach (string source in sources.Split(';'))
+                {
+                    switch (source)
+                    {
+                        case "null":
+                            l.Add(null);
+                            break;
+
+                        case "":
+                            l.Add(new string[0]);
+                            break;
+
+                        default:
+                            l.Add(source.Split(','));
+                            break;
+                    }
+                }
+            }
+
+            var expectedArr = expected == "" ? new string[0] : expected.Split(',');
+            var actual = CollectionUtil.Concat(l.ToArray());
+            CollectionAssert.AreEqual(expectedArr, actual);
+        }
+    }
+}
