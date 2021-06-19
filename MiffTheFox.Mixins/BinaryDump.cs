16802a7928e2c042c5dd1af8e61427192b7dc7e7
Miff <miff@miffthefox.info>
2021-06-19 18:10:01 -0400

    Add BinaryDump and TokenString

---
+++ MiffTheFox.Mixins/BinaryDump.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MiffTheFox
+{
+    // TODO: test this class
+
+    /// <summary>
+    /// Formats binary information for console output as hex. (Compare Unix xxd)
+    /// </summary>
+    public static class BinaryDump
+    {
+        public static void Dump(byte[] data, TextWriter target, Options options = null)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (target is null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            bool showHeaders = options?.ShowHeaders ?? true;
+            bool showAscii = options?.ShowASCII ?? true;
+
+            string hformat = options?.Uppercase ?? true ? "X" : "x";
+            string format = hformat + "2";
+            string aformat = null;
+
+            if (showHeaders)
+            {
+                int leftWidth = Math.Max(data.Length.ToString("X").Length, 2);
+                aformat = $"{{0,{leftWidth}:{hformat}}}-   ";
+
+                target.Write(new string(' ', leftWidth + 3));
+
+                for (int i = 0; i < 16; i++)
+                {
+                    target.Write(" -");
+                    target.Write(i.ToString(hformat));
+                }
+
+                target.WriteLine();
+            }
+
+            for (int i = 0; i < data.Length; i += 16)
+            {
+                if (showHeaders)
+                {
+                    target.Write(aformat, i >> 4);
+                }
+
+                for (int j = 0; j < 16; j++)
+                {
+                    target.Write((i + j < data.Length) ? data[i + j].ToString(format) : "  ");
+                    target.Write(' ');
+                }
+
+                if (showAscii)
+                {
+                    target.Write("   ");
+                    for (int j = 0; j < 16 && (i + j) < data.Length; j++)
+                    {
+                        byte b = data[i + j];
+                        target.Write(b >= ' ' && b <= '~' ? (char)b : '.');
+                    }
+                }
+
+                target.WriteLine();
+            }
+        }
+
+        public static void DumpToConsole(byte[] data, Options options = null)
+        {
+            Dump(data, Console.Out, options);
+        }
+
+        public class Options
+        {
+            public bool ShowHeaders { get; set; } = true;
+            public bool ShowASCII { get; set; } = true;
+            public bool Uppercase { get; set; } = true;
+        }
+    }
+}
---
+++ MiffTheFox.Mixins/Text/TokenString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Text
+{
+    public readonly struct TokenString
+    {
+        private readonly TokenStringToken[] _Tokens;
+
+        public TokenString(string baseString, char openToken = '{', char closeToken = '}')
+        {
+            int tokenCount = 1;
+            for (int i = 0; i < baseString.Length; i++)
+            {
+                if (baseString[i] == openToken || baseString[i] == closeToken)
+                {
+                    tokenCount++;
+                }
+            }
+
+            var tokenData = new TokenStringToken[tokenCount];
+            int tokenDataIndex = 0;
+            int currentTokenStart = 0;
+            bool currentTokenSpecial = false;
+
+            for (int i = 0; i < baseString.Length; i++)
+            {
+                if (baseString[i] == openToken && !currentTokenSpecial)
+                {
+                    int l = i - currentTokenStart;
+                    if (l > 0)
+                    {
+                        tokenData[tokenDataIndex] = new TokenStringToken(baseString.Substring(currentTokenStart, l), false);
+                        currentTokenStart = i + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public readonly struct TokenStringToken
+    {
+        public string Text { get; }
+        public bool IsSpecialToken { get; }
+
+        public TokenStringToken(string text, bool isSpecialToken)
+        {
+            Text = text;
+            IsSpecialToken = isSpecialToken;
+        }
+    }
+}
