12abb3847c78eda001cc49c89ea9a3e4f7cd7d2c
Miff <miff@miffthefox.info>
2021-06-19 18:22:01 -0400

    Add MiffTheFox.Collections.Enumerable.TryFirst

---
+++ MiffTheFox.Mixins/Collections/Enumerable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Collections
+{
+    internal static class Enumerable
+    {
+        /// <summary>
+        /// Tries to retrieve the first item in the sequence, returning false if the sequence is empty.
+        /// </summary>
+        internal static bool TryFirst<T>(this IEnumerable<T> enumerable, out T value)
+        {
+            // This code is based on the code used in .NET's internal
+            // System.Linq.Enuemrable.TryGetFirst<TSource> method.
+            // Why the heck is this method internal? It's useful!
+
+            if (enumerable is null)
+            {
+                throw new ArgumentNullException(nameof(enumerable));
+            }
+            else if (enumerable is IList<T> list)
+            {
+                if (list.Count > 0)
+                {
+                    value = list[0];
+                    return true;
+                }
+                else
+                {
+                    value = default;
+                    return false;
+                }
+            }
+            else
+            {
+                using (var e = enumerable.GetEnumerator())
+                {
+                    if (e.MoveNext())
+                    {
+                        value = e.Current;
+                        return true;
+                    }
+                    else
+                    {
+                        value = default;
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MixinTests/Collections/EnumerableTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox.Collections;
+using System;
+
+namespace MixinTests.Collections
+{
+    [TestClass]
+    public class EnumerableTests
+    {
+        [TestMethod]
+        [DataRow("1,2,3", true, "1")]
+        [DataRow("X,Y", true, "X")]
+        [DataRow("", false, null)]
+        public void TestTryFirstWithList(string haystack, bool expectedReturn, string expectedValue)
+        {
+            var h = haystack.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            bool r = Enumerable.TryFirst(h, out var value);
+            Assert.AreEqual(expectedReturn, r);
+            if (r) Assert.AreEqual(expectedValue, value);
+        }
+
+        [TestMethod]
+        [DataRow("AAA,BBB,CCC", "B", true, "BBB")]
+        [DataRow("AAA,X,CCC", "B", false, null)]
+        [DataRow("test1,test2,test3", "t3", true, "test3")]
+        [DataRow("", "X", false, null)]
+        public void TestTryFirstWithLinqWhere(string haystack, string needle, bool expectedReturn, string expectedValue)
+        {
+            var h = haystack.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            bool r = Enumerable.TryFirst(System.Linq.Enumerable.Where(h, x => x.Contains(needle)), out var value);
+            Assert.AreEqual(expectedReturn, r);
+            if (r) Assert.AreEqual(expectedValue, value);
+        }
+    }
+}
