0e6c2c342c0ef982591871b08d9940933007940d
Miff <miff@miffthefox.info>
2022-02-21 10:36:39 -0500

    Add StringBuilder.AppendJoin

---
+++ MiffTheFox.Mixins/Text/StringBuilderAppendJoin.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Text
+{
+    internal static class StringBuilderAppendJoin
+    {
+        public static StringBuilder AppendJoin(this StringBuilder builder, IEnumerable<string> strings, char separator)
+        {
+            if (strings != null)
+            {
+                bool first = true;
+                foreach (string s in strings)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        builder.Append(separator);
+                    }
+
+                    builder.Append(s);
+                }
+            }
+            return builder;
+        }
+
+        public static StringBuilder AppendJoin(this StringBuilder builder, IEnumerable<string> strings, string separator)
+        {
+
+            if (strings != null)
+            {
+                bool first = true;
+                foreach (string s in strings)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        builder.Append(separator);
+                    }
+
+                    builder.Append(s);
+                }
+            }
+            return builder;
+        }
+
+        public static StringBuilder AppendJoin<T>(this StringBuilder builder, IEnumerable<T> strings, char separator)
+        {
+            if (strings != null)
+            {
+                bool first = true;
+                foreach (T s in strings)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        builder.Append(separator);
+                    }
+
+                    if (s != null)
+                    {
+                        builder.Append(s.ToString());
+                    }
+                }
+            }
+            return builder;
+        }
+
+        public static StringBuilder AppendJoin<T>(this StringBuilder builder, IEnumerable<T> strings, string separator)
+        {
+            if (strings != null)
+            {
+                bool first = true;
+                foreach (T s in strings)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        builder.Append(separator);
+                    }
+
+                    if (s != null)
+                    {
+                        builder.Append(s.ToString());
+                    }
+                }
+            }
+            return builder;
+        }
+    }
+}
---
+++ MixinTests/Text/StringBuilderAppendJoinTests.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiffTheFox.Text;
+using System.Globalization;
+
+namespace MixinTests.Text
+{
+    [TestClass]
+    public class StringBuilderAppendJoinTests
+    {
+        [TestMethod]
+        [DataRow("1,2,3", '_', "1_2_3")]
+        [DataRow("abc,def,g,h", ' ', "abc def g h")]
+        [DataRow("0,,1", ';', "0;;1")]
+        public void TestJoinStringChar(string strings, char seperator, string expected)
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(strings?.Split(new char[] { ',' }), seperator);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinStringCharEmpty()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(Array.Empty<string>(), ',');
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinStringCharNull()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin((string[])null, ',');
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("1,2,3", "_", "1_2_3")]
+        [DataRow("abc,def,g,h", "||", "abc||def||g||h")]
+        [DataRow("0,,1", "xy", "0xyxy1")]
+        [DataRow("h,e,l,l,o", "", "hello")]
+        public void TestJoinStringString(string strings, string seperator, string expected)
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(strings?.Split(new char[] { ',' }), seperator);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinStringStringEmpty()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(Array.Empty<string>(), "||");
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinStringStringNull()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin((string[])null, "||");
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("1,2,3", '_', "1_2_3")]
+        [DataRow("0,,1", ';', "0;;1")]
+        public void TestJoinTChar(string strings, char seperator, string expected)
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(strings?.Split(new char[] { ',' })?.Select(x => string.IsNullOrEmpty(x) ? (int?)null : int.Parse(x, NumberStyles.None, CultureInfo.InvariantCulture)), seperator);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinTCharEmpty()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(Array.Empty<int>(), ',');
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinTCharNull()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin((int[])null, ',');
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("1,2,3", "_", "1_2_3")]
+        [DataRow("0,,1", "xy", "0xyxy1")]
+        [DataRow("1,2,3,4,5", "", "12345")]
+        public void TestJoinTString(string strings, string seperator, string expected)
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(strings?.Split(new char[] { ',' })?.Select(x => string.IsNullOrEmpty(x) ? (int?)null : int.Parse(x, NumberStyles.None, CultureInfo.InvariantCulture)), seperator);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinTStringEmpty()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin(Array.Empty<int>(), "||");
+            Assert.AreEqual("", sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestJoinTStringNull()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin((int[])null, "||");
+            Assert.AreEqual("", sb.ToString());
+        }
+    }
+}
