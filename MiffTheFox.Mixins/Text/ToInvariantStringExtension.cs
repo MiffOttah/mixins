14d2383dd38d20dff747baf71075c8d1ba0703a5
Miff <miff@miffthefox.info>
2022-02-21 10:36:32 -0500

    Add ToInvariantString

---
+++ MiffTheFox.Mixins/Text/ToInvariantStringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace MiffTheFox.Text
+{
+    internal static class ToInvariantStringExtension
+    {
+        public static string ToInvariantString(this object o)
+        {
+            switch (o)
+            {
+                case null:
+                case "":
+                    return string.Empty;
+
+                case IFormattable f:
+                    return f.ToString(null, CultureInfo.InvariantCulture);
+
+                default:
+                    return o.ToString();
+            }
+        }
+    }
+}
