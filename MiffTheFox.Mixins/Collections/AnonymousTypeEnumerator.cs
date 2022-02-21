4e1c8c199694deeb04a9e273469294916de25329
Miff <miff@miffthefox.info>
2022-02-21 09:44:57 -0500

    Add AnonymousTypeEnumerator

---
+++ MiffTheFox.Mixins/Collections/AnonymousTypeEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests.Collections
+{
+    internal static class AnonymousTypeEnumerator
+    {
+        internal static IEnumerable<KeyValuePair<string, object>> Enumerate(object obj)
+        {
+            if (obj != null)
+            {
+                var props = obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                
+                for (int i = 0; i < props.Length; i++)
+                {
+                    yield return new KeyValuePair<string, object>(props[i].Name, props[i].GetValue(obj));
+                }
+            }
+        }
+    }
+}
---
+++ MixinTests/Collections/AnonymousTypeEnumeratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests.Collections
+{
+    [TestClass]
+    public class AnonymousTypeEnumeratorTests
+    {
+        [TestMethod]
+        public void TestEnumeration()
+        {
+            var anon = new
+            {
+                Test1 = 1,
+                Test2 = 2
+            };
+
+            var r = AnonymousTypeEnumerator.Enumerate(anon).ToDictionary(x => x.Key, x => x.Value);
+            Assert.AreEqual(2, r.Count);
+            CollectionAssert.AreEquivalent(new string[] { "Test1", "Test2" }, r.Keys);
+            Assert.AreEqual(1, r["Test1"]);
+            Assert.AreEqual(2, r["Test2"]);
+        }
+
+        [TestMethod]
+        public void TestNullEnumeration()
+        {
+            var r = AnonymousTypeEnumerator.Enumerate(null).ToArray();
+            Assert.AreEqual(0, r.Length);
+        }
+    }
+}
