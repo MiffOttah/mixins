4a0966646a946e3fab791baa52d2822709641b7c
Miff <miff@miffthefox.info>
2022-02-21 09:46:49 -0500

    Fix namespace

--- MixinTests/Collections/AnonymousTypeEnumeratorTests.cs
+++ MixinTests/Collections/AnonymousTypeEnumeratorTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MixinTests.Collections
+namespace MiffTheFox.Collections
 {
     [TestClass]
     public class AnonymousTypeEnumeratorTests
