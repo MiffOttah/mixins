f89b0a88c61a456e3123ecfc13f7f047d5b8eec6
Miff <miff@miffthefox.info>
2022-10-27 20:11:43 -0400

    Add AspectScale

---
+++ MiffTheFox.Mixins/Geometry/AspectScale.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Geometry
+{
+    internal static class AspectScale
+    {
+        internal static void ScaleToFitSquare(int targetSize, int srcWidth, int srcHeight, out int scaledWidth, out int scaledHeight)
+        {
+            if (targetSize <= 0 || srcWidth <= 0 || srcHeight <= 0) throw new ArgumentException("Sizes must be positive.");
+
+            double scale;
+            if (srcWidth > srcHeight)
+            {
+                scale = (double)targetSize / srcWidth;
+            }
+            else
+            {
+                scale = (double)targetSize / srcHeight;
+            }
+
+            scaledWidth = (int)Math.Round(scale * srcWidth);
+            scaledHeight = (int)Math.Round(scale * srcHeight);
+        }
+    }
+}
---
+++ MixinTests/Geometry/AspectScaleTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixinTests.Geometry
+{
+    [TestClass]
+    public class AspectScaleTests
+    {
+        [TestMethod]
+        [DataRow(100, 10, 5, 100, 50)]
+        [DataRow(100, 5, 10, 50, 100)]
+        [DataRow(10, 100, 40, 10, 4)]
+        [DataRow(10, 40, 100, 4, 10)]
+        [DataRow(100, 50, 50, 100, 100)]
+        [DataRow(100, 100, 50, 100, 50)]
+        [DataRow(100, 50, 100, 50, 100)]
+        [DataRow(100, 100, 100, 100, 100)]
+        [DataRow(100, 200, 100, 100, 50)]
+        [DataRow(100, 100, 200, 50, 100)]
+        public void TestScaleToFixBox(int targetSize, int srcWidth, int srcHeight, int expectedScaledWidth, int expectedScaledHeight)
+        {
+            AspectScale.ScaleToFitSquare(targetSize, srcWidth, srcHeight, out int scaledWidth, out int scaledHeight);
+            Assert.AreEqual(expectedScaledWidth, scaledWidth);
+            Assert.AreEqual(expectedScaledHeight, scaledHeight);
+        }
+    }
+}
